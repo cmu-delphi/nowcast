4b6bbdeeec337fdc25ab66f423fd3023f8cc368e
Sangwon Hyun <shyun@cmu.edu>
2017-12-21 04:09:21 -0500

    sensor_update_justin.

--- src/sensors/sensor_update_justin.py
+++ src/sensors/sensor_update_justin.py
@@ -339,8 +339,7 @@ def get_ghtj(location, epiweek, valid):
   loc = 'US' if location == 'nat' else location
   def justinfun(location, epiweek):
     main_driver = '/home/automation/ghtj/ghtj.R'   ### Need to set an absolute path
-    print('here0')
-    subprocess.check_call(['Rscript', main_driver, location, epiweek], shell=False)
+    subprocess.check_call(['Rscript', main_driver, location, str(epiweek)], shell=False)
     outputdir = '/home/automation/ghtj/' ### Need to set an absolute path
     prefix = 'ghtpred'
     predfilename = outputdir + prefix + '-'+ location +'-' + str(epiweek) + '.txt'
