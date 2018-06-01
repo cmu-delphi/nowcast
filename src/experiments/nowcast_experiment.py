d463717abe87226177d2e47d784a3c50a7001c39
undefx <dfarrow0@gmail.com>
2018-06-01 17:16:11 -0500

    introduce retrospective experiments
    Retrospective nowcasting for multiple experimental protocols, including:
    
      - ablation: removal of individual sensors
      - abscission: removal of geographic tiers
        - option 1: nowcast 1 season using 8+ sensors
        - option 2: nowcast 6+ seasons using 3 sensors
      - covariance: use different covariance estimation methods
      - vanilla: the operational nowcast, as a control and sanity check
    
    Unit tests are included.

---
+++ src/experiments/nowcast_experiment.py
@@ -0,0 +1,288 @@
+"""
+===============
+=== Purpose ===
+===============
+
+Retrospective nowcasting for multiple experimental protocols, including:
+  - ablation: removal of individual sensors
+  - abscission: removal of geographic tiers
+    - option 1: nowcast 1 season using 8+ sensors
+    - option 2: nowcast 6+ seasons using 3 sensors
+  - covariance: use different covariance estimation methods
+  - vanilla: the operational nowcast, as a control and sanity check
+
+The operational nowcast (the "vanilla" experiment) should be equivalent to:
+  - the "covariance" experiment with parameter "bd2", corresponding to the
+    covariance estimation method "BlendDiagonal2" in ../fusion/covariance.py
+  - the "abscise1" experiment with parameter "state", which means to use all
+    sensors at all locations (for the short period of time during which all
+    8 sensors were simultaneously available)
+"""
+
+# standard library
+import argparse
+import csv
+
+# first party
+from delphi.epidata.client.delphi_epidata import Epidata
+from delphi.nowcast.fusion import covariance
+from delphi.nowcast.fusion.nowcast import Nowcast
+from delphi.nowcast.util.flu_data_source import FluDataSource
+from delphi.operations import secrets
+from delphi.utils.epiweek import range_epiweeks
+from delphi.utils.geo.locations import Locations
+
+
+class InsufficientDataException(Exception):
+  """An Exception indicating that there is not enough training data."""
+
+
+class UnknownSensorException(Exception):
+  """An Exception indicating that a sensor is unknown."""
+
+
+class InvalidExperimentException(Exception):
+  """An Exception indicating that an experiment is invalid."""
+
+
+class NowcastExperiment:
+  """Produces retrospective nowcasts of ILI in the US."""
+
+  class Provider:
+    """
+    Provides instances for the experiment; can be overridden in unit tests.
+    """
+
+    def get_data_source(self, epidata, sensors, locations):
+      """Return a FluDataSource instance."""
+      return FluDataSource(epidata, sensors, locations)
+
+    def get_nowcast(self, data_source, cov_impl):
+      """Return a Nowcast instance."""
+      return Nowcast(
+          data_source,
+          shrinkage=cov_impl,
+          min_observations=NowcastExperiment.MIN_OBSERVATIONS)
+
+    def call_with_file(filename, func):
+      """Run the given function with a file object."""
+      with open(filename, 'w', newline='') as file_obj:
+        func(file_obj)
+
+  # the number of weekly observations required to start making nowcasts
+  MIN_OBSERVATIONS = 5
+
+  @staticmethod
+  def new_instance():
+    return NowcastExperiment(
+        NowcastExperiment.Provider(), Epidata, FluDataSource.new_instance())
+
+  def __init__(self, provider, epidata, data_source):
+    self.provider = provider
+    self.epidata = epidata
+    self.data_source = data_source
+
+  @staticmethod
+  def get_locations_at_resolution(resolution):
+    """
+    Return a list of locations in which sensor readings should be available.
+    """
+
+    # all experiments include (a copy of) the national tier
+    locations = Locations.nat_list[::]
+
+    if resolution in ('regional', 'state'):
+      # all types of regions
+      locations.extend(Locations.hhs_list)
+      locations.extend(Locations.cen_list)
+
+    if resolution == 'state':
+      # state and below
+      locations.extend(Locations.ny_state_list)
+      locations.extend(Locations.atom_list)
+
+    return locations
+
+  def get_weeks_in_nowcast(self, sensor, min_observations):
+    """Return a list of weeks on which nowcasts should include this sensor."""
+
+    # get the range of possible nowcast weeks
+    all_weeks = self.data_source.get_weeks()
+    week_range = self.epidata.range(min(all_weeks), max(all_weeks))
+
+    # get sensor data for US nationally on those weeks
+    response = self.epidata.sensors(
+        secrets.api.sensors, sensor, 'nat', week_range)
+    rows = self.epidata.check(response)
+
+    # extract the weeks from the returned data
+    sensor_weeks = sorted(r['epiweek'] for r in rows)
+    if len(sensor_weeks) <= min_observations:
+      msg = 'sensor %s available <= %d weeks' % (sensor, min_observations)
+      raise InsufficientDataException(msg)
+
+    # return nowcast-able weeks based on this sensor
+    return sensor_weeks[min_observations:]
+
+  def get_ablation_parameters(self, ablate):
+    """Return parameters for the 'ablation' experiment."""
+
+    # all except user-specified sensor; all locations; weeks on which the
+    # held-out sensor would be included in the nowcast; default covariance
+    # method
+    if ablate not in FluDataSource.SENSORS:
+      raise UnknownSensorException('unknown sensor: %s' % ablate)
+    sensors = [s for s in FluDataSource.SENSORS if s != ablate]
+    locations = Locations.region_list
+    weeks = self.get_weeks_in_nowcast(
+        ablate, NowcastExperiment.MIN_OBSERVATIONS)
+    cov_impl = covariance.BlendDiagonal2
+    return sensors, locations, weeks, cov_impl
+
+  def get_abscission1_parameters(self, abscise1):
+    """Return parameters for the 'abscission1' experiment."""
+
+    # all sensors; user-specified location tier; weeks on which all sensors
+    # were available; default covariance method
+    sensors = FluDataSource.SENSORS
+    locations = NowcastExperiment.get_locations_at_resolution(abscise1)
+    weeks = list(range_epiweeks(201445, 201520, inclusive=True))
+    cov_impl = covariance.BlendDiagonal2
+    return sensors, locations, weeks, cov_impl
+
+  def get_abscission2_parameters(self, abscise2):
+    """Return parameters for the 'abscission2' experiment."""
+
+    # high-resolution sensors; user-specified location tier; weeks on which
+    # sensors are all available; default covariance method
+    sensors = ['twtr', 'cdc', 'sar3']
+    locations = NowcastExperiment.get_locations_at_resolution(abscise2)
+    latest_week = self.data_source.get_most_recent_issue()
+    weeks = list(range_epiweeks(201330, latest_week, inclusive=True))
+    cov_impl = covariance.BlendDiagonal2
+    return sensors, locations, weeks, cov_impl
+
+  def get_covariance_parameters(self, cov_name):
+    """Return parameters for the 'covariance' experiment."""
+
+    # all sensors; all locations; all weeks; user-specified covariance method
+    # (see ../fusion/covariance.py)
+    sensors = FluDataSource.SENSORS
+    locations = Locations.region_list
+    weeks = self.data_source.get_weeks()
+    cov_impl = {
+      'bd0': covariance.BlendDiagonal0,
+      'bd1': covariance.BlendDiagonal1,
+      'bd2': covariance.BlendDiagonal2,
+    }[cov_name]
+    return sensors, locations, weeks, cov_impl
+
+  def get_vanilla_parameters(self):
+    """Return parameters for the 'vanilla' experiment."""
+
+    # all sensors; all locations; all weeks; default covariance method
+    sensors = FluDataSource.SENSORS
+    locations = Locations.region_list
+    weeks = self.data_source.get_weeks()
+    cov_impl = covariance.BlendDiagonal2
+    return sensors, locations, weeks, cov_impl
+
+  def get_values_for_experiment(
+      self,
+      ablate=None,
+      abscise1=None,
+      abscise2=None,
+      covariance=None,
+      vanilla=None):
+    """Return parameters corresponding to the given experiment."""
+
+    if ablate:
+      return self.get_ablation_parameters(ablate)
+    if abscise1:
+      return self.get_abscission1_parameters(abscise1)
+    if abscise2:
+      return self.get_abscission2_parameters(abscise2)
+    if covariance:
+      return self.get_covariance_parameters(covariance)
+    if vanilla:
+      return self.get_vanilla_parameters()
+
+  def save_to_file(self, file_obj, weeks, nowcasts):
+    """Extract and save the results."""
+    writer = csv.writer(file_obj)
+    for week, nowcast in zip(weeks, nowcasts):
+      for location, value, stdev in nowcast:
+        writer.writerow([week, location, float(value), float(stdev)])
+
+  def run_experiment(
+      self, filename, ablate, abscise1, abscise2, covariance, vanilla):
+    """
+    Produce and save nowcasts as indicated by the given experiment parameters.
+    """
+
+    # get parameters for this experiment
+    sensors, locations, weeks, cov_impl = self.get_values_for_experiment(
+        ablate, abscise1, abscise2, covariance, vanilla)
+
+    # prefetch bulk data
+    data_source = self.provider.get_data_source(
+        self.epidata, sensors, locations)
+    data_source.prefetch(max(weeks))
+
+    # compute the nowcasts
+    nowcaster = self.provider.get_nowcast(data_source, cov_impl)
+    nowcasts = nowcaster.batch_nowcast(weeks)
+
+    # save the nowcasts
+    print('saving nowcasts to %s' % filename)
+    func = lambda file_obj: self.save_to_file(file_obj, weeks, nowcasts)
+    self.provider.call_with_file(func)
+
+
+def get_argument_parser():
+  """Define command line arguments and usage."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      'filename',
+      help='output filename (*.csv)')
+  parser.add_argument(
+      '--ablate',
+      choices=FluDataSource.SENSORS,
+      help='ablation experiment, leaving out this sensor')
+  parser.add_argument(
+      '--abscise1',
+      choices=('national', 'regional', 'state'),
+      help='abscission experiment (all sensors), with this resolution')
+  parser.add_argument(
+      '--abscise2',
+      choices=('national', 'regional', 'state'),
+      help='abscission experiment (hi-res sensors), with this resolution')
+  parser.add_argument(
+      '--covariance',
+      choices=('bd0', 'bd1', 'bd2'),
+      help='covariance experiment, using this algorithm')
+  parser.add_argument(
+      '--vanilla',
+      default=None,
+      action='store_true',
+      help='a control; unmodified operational nowcasting')
+  return parser
+
+
+def validate_args(args):
+  """Validate and return command line arguments."""
+  values = [
+    args.ablate, args.abscise1, args.abscise2, args.covariance, args.vanilla
+  ]
+  if sum(v is not None for v in values) != 1:
+    raise InvalidExperimentException('exactly one experiment must be run')
+  return [args.filename] + values
+
+
+def main(*args):
+  """Run this script from the command line."""
+  NowcastExperiment.run_experiment(*args)
+
+
+if __name__ == '__main__':
+  main(*validate_args(get_argument_parser().parse_args()))
---
+++ tests/experiments/test_nowcast_experiment.py
@@ -0,0 +1,291 @@
+"""Unit tests for nowcast_experiment.py."""
+
+# standard library
+import unittest
+from unittest.mock import MagicMock
+
+# first party
+from delphi.nowcast.fusion import covariance
+from delphi.nowcast.fusion.nowcast import Nowcast
+from delphi.nowcast.util.flu_data_source import FluDataSource
+from delphi.utils.geo.locations import Locations
+
+# py3tester coverage target
+__test_target__ = 'delphi.nowcast.experiments.nowcast_experiment'
+
+
+class UnitTests(unittest.TestCase):
+  """Basic unit tests."""
+
+  def setUp(self):
+    self.mock_provider = MagicMock()
+    self.mock_epidata = MagicMock()
+    self.mock_data_source = MagicMock()
+    self.experiment = NowcastExperiment(
+        self.mock_provider, self.mock_epidata, self.mock_data_source)
+
+  def test_get_argument_parser(self):
+    """An ArgumentParser should be returned."""
+    self.assertIsInstance(get_argument_parser(), argparse.ArgumentParser)
+
+  def test_validate_args(self):
+    """Arguments should be validated."""
+
+    new_args = lambda: MagicMock(
+        ablate=None,
+        abscise1=None,
+        abscise2=None,
+        covariance=None,
+        vanilla=None,
+        filename='out.csv')
+
+    with self.subTest(name='no experiment given'):
+      args = new_args()
+      with self.assertRaises(InvalidExperimentException):
+        validate_args(args)
+
+    with self.subTest(name='two experiments given'):
+      args = new_args()
+      args.ablate = 'gft'
+      args.abscise1 = 'state'
+      with self.assertRaises(InvalidExperimentException):
+        validate_args(args)
+
+    with self.subTest(name='valid example'):
+      args = new_args()
+      args.vanilla = True
+      expected = ['out.csv', None, None, None, None, True]
+      self.assertEqual(validate_args(args), expected)
+
+  def test_nowcast_experiment_new_instance(self):
+    """Create a NowcastExperiment instance with default parameters."""
+    inst = NowcastExperiment.new_instance()
+    self.assertIsInstance(inst, NowcastExperiment)
+
+  def test_default_provider(self):
+    """Provide default instances of required objects."""
+
+    provider = NowcastExperiment.Provider()
+    data_source = provider.get_data_source(None, None, None)
+    nowcast = provider.get_nowcast(None, None)
+
+    self.assertIsInstance(data_source, FluDataSource)
+    self.assertIsInstance(nowcast, Nowcast)
+
+  def test_get_locations_at_resolution(self):
+    """Return locations needed for abscission experiments."""
+
+    with self.subTest(name='national'):
+      locations = NowcastExperiment.get_locations_at_resolution('national')
+      self.assertEqual(locations, Locations.nat_list)
+
+    with self.subTest(name='regional'):
+      locations = NowcastExperiment.get_locations_at_resolution('regional')
+      expected = Locations.nat_list + Locations.hhs_list + Locations.cen_list
+      self.assertEqual(locations, expected)
+
+    with self.subTest(name='state'):
+      locations = NowcastExperiment.get_locations_at_resolution('state')
+      self.assertEqual(locations, Locations.region_list)
+
+  def test_get_weeks_in_nowcast(self):
+    """Return weeks on which a sensor will be included in the nowcast."""
+
+    self.mock_data_source.get_weeks.return_value = [2, 3, 4, 5, 6, 7]
+
+    def set_fake_epidata(weeks):
+      self.mock_epidata.check.return_value = [{'epiweek': w} for w in weeks]
+
+    with self.subTest(name='enough historical data'):
+      set_fake_epidata([3, 5, 7])
+      result = self.experiment.get_weeks_in_nowcast('sar3', 2)
+      self.assertEqual(result, [7])
+
+    with self.subTest(name='not enough historical data'):
+      set_fake_epidata([4, 6])
+      with self.assertRaises(InsufficientDataException):
+        self.experiment.get_weeks_in_nowcast('sar3', 2)
+
+  def test_get_ablation_parameters_requires_valid_sensor(self):
+    """Raise Exception for invalid sensor."""
+
+    with self.assertRaises(UnknownSensorException):
+      self.experiment.get_ablation_parameters('x')
+
+  def test_get_ablation_parameters(self):
+    """Return sensible ablation parameters."""
+
+    week_list = list(range(201801, 201809))
+    self.mock_data_source.get_weeks.return_value = week_list
+    self.mock_epidata.check.return_value = [{'epiweek': w} for w in week_list]
+
+    params = self.experiment.get_ablation_parameters('arch')
+    sensors, locations, weeks, cov_impl = params
+
+    actual_sensors = set(sensors)
+    expected_sensors = set(FluDataSource.SENSORS) - {'arch'}
+    self.assertEqual(actual_sensors, expected_sensors)
+    self.assertEqual(locations, Locations.region_list)
+    self.assertEqual(weeks, week_list[NowcastExperiment.MIN_OBSERVATIONS:])
+    self.assertEqual(cov_impl, covariance.BlendDiagonal2)
+
+  def test_get_abscission1_parameters(self):
+    """Return sensible abscission1 parameters."""
+
+    params = self.experiment.get_abscission1_parameters('national')
+    sensors, locations, weeks, cov_impl = params
+
+    self.assertEqual(sensors, FluDataSource.SENSORS)
+    self.assertEqual(locations, Locations.nat_list)
+    self.assertIn(201445, weeks)
+    self.assertIn(201520, weeks)
+    self.assertEqual(cov_impl, covariance.BlendDiagonal2)
+
+  def test_get_abscission2_parameters(self):
+    """Return sensible abscission2 parameters."""
+
+    self.mock_data_source.get_most_recent_issue.return_value = 201820
+
+    params = self.experiment.get_abscission2_parameters('national')
+    sensors, locations, weeks, cov_impl = params
+
+    self.assertEqual(set(sensors), set(['cdc', 'sar3', 'twtr']))
+    self.assertEqual(locations, Locations.nat_list)
+    self.assertIn(201330, weeks)
+    self.assertIn(201820, weeks)
+    self.assertEqual(cov_impl, covariance.BlendDiagonal2)
+
+  def test_get_covariance_parameters(self):
+    """Return sensible covariance parameters."""
+
+    self.mock_data_source.get_weeks.return_value = [1, 2, 3]
+
+    params = self.experiment.get_covariance_parameters('bd0')
+    sensors, locations, weeks, cov_impl = params
+
+    self.assertEqual(sensors, FluDataSource.SENSORS)
+    self.assertEqual(locations, Locations.region_list)
+    self.assertEqual(weeks, [1, 2, 3])
+    self.assertEqual(cov_impl, covariance.BlendDiagonal0)
+
+  def test_get_vanilla_parameters(self):
+    """Return parameters used in operational nowcasting."""
+
+    self.mock_data_source.get_weeks.return_value = [1, 2, 3]
+
+    params = self.experiment.get_vanilla_parameters()
+    sensors, locations, weeks, cov_impl = params
+
+    self.assertEqual(sensors, FluDataSource.SENSORS)
+    self.assertEqual(locations, Locations.region_list)
+    self.assertEqual(weeks, [1, 2, 3])
+    self.assertEqual(cov_impl, covariance.BlendDiagonal2)
+
+  def test_get_values_for_experiment(self):
+    """Return parameters for each experiment type."""
+
+    week_list = list(range(201801, 201810))
+    self.mock_data_source.get_most_recent_issue.return_value = max(week_list)
+    self.mock_data_source.get_weeks.return_value = week_list
+    self.mock_epidata.check.return_value = [{'epiweek': w} for w in week_list]
+
+    with self.subTest(name='ablate'):
+      params = self.experiment.get_values_for_experiment(ablate='gft')
+      self.assertEqual(len(params), 4)
+      self.assertNotIn('gft', params[0])
+      self.assertIn('ght', params[0])
+
+    with self.subTest(name='abscise1'):
+      params = self.experiment.get_values_for_experiment(abscise1='regional')
+      self.assertEqual(len(params), 4)
+      self.assertNotIn('tx', params[1])
+      self.assertIn('hhs6', params[1])
+
+    with self.subTest(name='abscise2'):
+      params = self.experiment.get_values_for_experiment(abscise2='regional')
+      self.assertEqual(len(params), 4)
+      self.assertNotIn('tx', params[1])
+      self.assertIn('hhs6', params[1])
+
+    with self.subTest(name='covariance'):
+      params = self.experiment.get_values_for_experiment(covariance='bd1')
+      self.assertEqual(len(params), 4)
+      self.assertEqual(params[3], covariance.BlendDiagonal1)
+
+    with self.subTest(name='vanilla'):
+      params = self.experiment.get_values_for_experiment(vanilla=True)
+      self.assertEqual(len(params), 4)
+      self.assertEqual(params[0], FluDataSource.SENSORS)
+      self.assertIn('tx', params[1])
+      self.assertEqual(params[2][-1], 201809)
+      self.assertEqual(params[3], covariance.BlendDiagonal2)
+
+  def test_save_to_file(self):
+    """Save the nowcast to a CSV file."""
+
+    class FakeFile:
+
+      def __init__(self):
+        self.lines = []
+
+      def write(self, d):
+        self.lines.append(d.strip())
+
+    file_obj = FakeFile()
+    weeks = [50, 51, 52]
+    nowcasts = [
+      [('aa', 10, 20), ('ba', 30, 40)],
+      [('ab', 11, 21), ('bb', 31, 41)],
+      [('ac', 12, 22), ('bc', 32, 42)],
+    ]
+
+    self.experiment.save_to_file(file_obj, weeks, nowcasts)
+
+    self.assertEqual(file_obj.lines, [
+      '50,aa,10.0,20.0',
+      '50,ba,30.0,40.0',
+      '51,ab,11.0,21.0',
+      '51,bb,31.0,41.0',
+      '52,ac,12.0,22.0',
+      '52,bc,32.0,42.0',
+    ])
+
+  def test_run_experiment(self):
+    """Run an experiment end-to-end."""
+
+    # vanilla
+    args = ['filename'] + [None] * 4 + [True]
+    self.mock_data_source.get_weeks.return_value = [1, 2, 3]
+    self.experiment.run_experiment(*args)
+
+    self.assertTrue(self.mock_data_source.get_weeks.called)
+
+    self.assertTrue(self.mock_provider.get_data_source.called)
+    args, kwargs = self.mock_provider.get_data_source.call_args
+    self.assertTrue(len(args), 3)
+    self.assertEqual(args[0], self.mock_epidata)
+    self.assertIn('gft', args[1])
+    self.assertIn('nv', args[2])
+    data_source = self.mock_provider.get_data_source()
+
+    self.assertTrue(self.mock_provider.get_nowcast.called)
+    args, kwargs = self.mock_provider.get_nowcast.call_args
+    self.assertTrue(len(args), 2)
+    self.assertEqual(args[0], data_source)
+    self.assertEqual(args[1], covariance.BlendDiagonal2)
+    nowcaster = self.mock_provider.get_nowcast()
+
+    self.assertTrue(self.mock_provider.call_with_file.called)
+    args, kwargs = self.mock_provider.call_with_file.call_args
+    self.assertTrue(len(args), 1)
+    self.assertTrue(callable(args[0]))
+
+    self.assertTrue(data_source.prefetch.called)
+    args, kwargs = data_source.prefetch.call_args
+    self.assertTrue(len(args), 1)
+    self.assertEqual(args[0], 3)
+
+    self.assertTrue(nowcaster.batch_nowcast.called)
+    args, kwargs = nowcaster.batch_nowcast.call_args
+    self.assertTrue(len(args), 1)
+    self.assertEqual(args[0], [1, 2, 3])
